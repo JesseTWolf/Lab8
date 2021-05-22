9a03816ed5112b0848570dcea38013aed05dfa24
Jesse <jtwolf@ucsd.edu>
2021-05-22 00:29:42 -0700

    Completed router tests for part 1

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,25 @@
  * @jest-environment jsdom
  */
 
+import { pushToHistory } from "../scripts/router";
+
+// Test if the length of the history stack and the current state object is correct.
+ describe('pushToHistory test', () => {
+    let history;
+
+    test('settings page', () => {
+        history = pushToHistory('settings', '2');
+        expect(history.length).toBe(2);
+    });
+
+    test('entry page', () => {
+        history = pushToHistory('entry', '2');
+        expect(history.length).toBe(3);
+    })
+
+    test('other page', () => {
+        history = pushToHistory('', '2');
+        expect(history.length).toBe(4);
+    })
+
+});
